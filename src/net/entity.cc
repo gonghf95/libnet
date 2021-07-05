92076390e93429f556eb9f25d22f818ccd2a9ed2
gonghf95 <1342735006@qq.com>
2021-07-05 10:59:14 -0400

    Initial commit

--- src/net/entity.cc
+++
@@ -1,8 +0,0 @@
-#include "src/net/entity.h"
-#include "src/net/component.h"
-
-void Entity::addComponent(Component* component)
-{
-    component->setParent(this);
-    components_.push_back(component);
-}
